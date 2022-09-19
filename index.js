2f63e486efcf12ffcc50fd026e00e4e7f892a285
Maeilum <donathan.d7515@gmail.com>
2022-09-19 02:38:46 -0500

    created gameStart loop and scoreboard

--- index.js
+++ index.js
@@ -4,27 +4,45 @@ function getComputerChoice() {
   return choice;
 }
 
-const playerChoice = prompt('Pick A Weapon!').toLowerCase();
-const computerChoice = getComputerChoice();
-const score = 0;
+let Victor;
+let score = 0;
 
- function gameStart(playerChoice, computerChoice){
+function gameStart(playerChoice, computerChoice){
   if (playerChoice === computerChoice){
-    alert(`It's a tie!`);
+    alert(`No one wins!`);
+    Victor = null;
   }
-  else if (playerChoice === 'rock' && computerChoice === 'paper'){
-    alert('You lose! Too bad :/')
+  else if (
+    playerChoice === 'rock' && computerChoice === 'paper' ||
+    playerChoice === 'paper' && computerChoice === 'scissors' ||
+    playerChoice === 'scissors' && computerChoice === 'rock') {
+    Victor = false;
+    alert('You lose!')
+  } else {
+    Victor = true;
+    alert('You Win!')
+  } 
+}
+function game(){
+  for (let i=0; i<5; i++) {
+    let playerChoice = prompt('Pick A Weapon!').toLowerCase();
+    let computerChoice = getComputerChoice();
+    gameStart(playerChoice, computerChoice)
+
+    if (Victor === true){
+      ++score
+      console.log(score)
+    } else if (Victor === false){
+      --score
+      console.log(score)
+    }
+
+    console.log(playerChoice)
+    console.log(computerChoice)
   }
-  else if (playerChoice === 'scissors' && computerChoice === 'rock'){
-    alert('You lose! Too bad :/')
-  }
-  else if (playerChoice === 'paper' && computerChoice === 'scissors'){
-    alert('You lose! Too bad :/')
-  }
-  else { 
-    alert('You win! Nice job :)')
-  }
+
  }
  
-gameStart(playerChoice, computerChoice)
- 
+
+
+ game()
